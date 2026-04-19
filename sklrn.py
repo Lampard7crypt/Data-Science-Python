68a5d5ce77b1333d6b1e98472da01409a973bb38
Tyler <tk654001@gmail.com>
2026-04-19 13:55:58 +0300

    Ridge, Lasso and ElasticNet training. On California housing.

--- sklrn.py
+++ sklrn.py
@@ -1,20 +1,27 @@
 import numpy as np
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris, fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, mean_squared_error
+from sklearn.linear_model import Ridge, Lasso, ElasticNet
 
 # Load data (NumPy arrays)
-iris = load_iris()
-X, y = iris.data, iris.target
+cal = fetch_california_housing()
+X, y = cal.data, cal.target
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Train model
-clf = RandomForestClassifier()
-clf.fit(X_train, y_train)
+# Ridge, Lasso and ElasticNet then OneHotEncoder, OrdinalEncoder. 
+ridge = Ridge(alpha=0.1)
+lasso = Lasso(alpha=0.1)
+eln = ElasticNet(alpha=0.1)
 
-# Predict and evaluate
-predictions = clf.predict(X_test)
-print(accuracy_score(y_test, predictions))
+ridge.fit(X_train, y_train)
+lasso.fit(X_train, y_train)
+eln.fit(X_train, y_train)
+
+print(f"\nRidge R2: {ridge.score(X_test, y_test)}")
+print(f"Lasso R2: {lasso.score(X_test, y_test)}")
+print(f"eln R2: {eln.score(X_test, y_test)}\n")
+
